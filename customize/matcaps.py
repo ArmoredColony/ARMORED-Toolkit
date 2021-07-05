3898d887a99724165c27b314c56d5f936aa7952e
Armored Colony <www.armoredColony@gmail.com>
2021-07-05 12:38:57 -0500

    removed comments

--- customize/matcaps.py
+++ customize/matcaps.py
@@ -1,29 +1,19 @@
 # v0.1
 
 import bpy
-# import shutil
 import os
+import shutil
 
 from .. utils import addon
 
+
 debug = True
 
-# class ARMORED_OT_Resources(bpy.types.Operator):
-#     bl_idname = 'armored.customize'
-#     bl_label = 'MACHIN3: Customize'
-#     bl_description = 'Customize various Blender preferences, settings and keymaps.'
-#     bl_options = {'INTERNAL'}
 
-# resourcespath = os.path.join(os.path.dirname(__file__), 'matcaps')
-# print(f'Matcap Path: {resourcespath}')
+def get_source_matcaps():
+    matcap_source_path = os.path.join(addon.get_path(), 'resources', 'matcaps')
+    return os.listdir(matcap_source_path)
 
-# datafilespath = bpy.utils.user_resource('DATAFILES')
-# print(f'Data Files Path: {datafilespath}')
-
-# scriptspath = bpy.utils.user_resource('SCRIPTS')
-
-matcapsourcepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources', 'matcaps')
-original_matcaps = os.listdir(matcapsourcepath)
 
 def makedir(pathstring):
     import os
@@ -32,58 +22,50 @@ def makedir(pathstring):
         os.makedirs(pathstring)
     return pathstring
 
+
 def load_matcaps():
-    import os
-    import shutil
-
-    datafilespath = bpy.utils.user_resource('DATAFILES')
-    matcapsourcepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources', 'matcaps')
-
-    matcaptargetpath = makedir(os.path.join(datafilespath, 'studiolights', 'matcap'))
-    matcaps = os.listdir(matcapsourcepath)
-    installed_matcaps = set(os.listdir(matcaptargetpath))
-
+    data_files_path = bpy.utils.user_resource('DATAFILES', create=True)
+    matcap_target_path = makedir(os.path.join(data_files_path, 'studiolights', 'matcap'))
     
-    if debug:
-        print(f'installed matcaps {installed_matcaps}')
+    matcap_source_path = os.path.join(addon.get_path(), 'resources', 'matcaps')
+    source_matcaps = os.listdir(matcap_source_path)
 
-    for matcap in matcaps:
+    installed_matcaps = set(os.listdir(matcap_target_path))
+
+    for matcap in source_matcaps:
         if matcap in installed_matcaps:
             if debug:
                 print(f'matcap {matcap} already exists')
             continue
 
-        shutil.copy(os.path.join(matcapsourcepath, matcap), matcaptargetpath)
+        shutil.copy(os.path.join(matcap_source_path, matcap), matcap_target_path)
 
         if debug:
             print(f'Installed Matcap: {matcap}')
 
     bpy.context.preferences.studio_lights.refresh()
 
-    # if all([mc in matcaps for mc in ['matcap_base.exr', 'matcap_shiny_red.exr']]):
-        # preferences().switchmatcap1 = 'matcap_base.exr'
 
 def unload_matcaps():
-    import os
-    import shutil
+    data_files_path = bpy.utils.user_resource('DATAFILES', create=True)
+    matcap_target_path = makedir(os.path.join(data_files_path, 'studiolights', 'matcap'))
 
-    matcapsourcepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources', 'matcaps')
-    matcaps = os.listdir(matcapsourcepath)
+    matcap_source_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources', 'matcaps')
+    matcaps = os.listdir(matcap_source_path)
 
-    datafilespath = bpy.utils.user_resource('DATAFILES')
-    matcaptargetpath = makedir(os.path.join(datafilespath, 'studiolights', 'matcap'))
-    # matcaps = os.listdir(matcaptargetpath)
+    installed_matcaps = set(os.listdir(matcap_target_path))
 
     for matcap in matcaps:
-        os.remove(os.path.join(matcaptargetpath, matcap))
+        if matcap not in installed_matcaps:
+            if debug:
+                print(f'matcap {matcap} doesnt even exist')
+            continue
+
+        os.remove(os.path.join(matcap_target_path, matcap))
         if debug:
             print(f'Uninstalled Matcap: {matcap}')
 
     bpy.context.preferences.studio_lights.refresh()
-
-    # if all([mc in matcaps for mc in ['matcap_base.exr', 'matcap_shiny_red.exr']]):
-        # preferences().switchmatcap1 = 'matcap_base.exr'
-        # preferences().switchmatcap2 = 'matcap_shiny_red.exr'
 
 
 def register():
@@ -98,57 +80,4 @@ def register():
 def unregister():
 
     if isinstance(bpy.context.space_data, bpy.types.SpacePreferences):
-
-        # disabled = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop', 'Instance FALSE.txt')
-        # enabled = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop', 'Instance TRUE.txt')
-        # addon = 'ARMORED_Toolkit'
-        # enabled_addons = set(bpy.context.preferences.addons.keys())
-        # try:
-            # f = open(disabled, "x")
-        # except FileExistsError:
-            # pass
-
         unload_matcaps()
-
-    # if addon not in enabled_addons:
-        # print(f'Addon: {addon} is DISABLED')
-        # f = open(disabled, "x")
-
-        # delete_matcaps()
-    # else:
-        # print(f'Addon: {addon} is still ENABLED')
-        # f = open(enabled, "x")
-
-    # pass
-
-
-    # for cls in classes:
-        # bpy.utils.register_class(cls)
-
-    # for cls in classes:
-        # bpy.utils.unregister_class(cls)
-
-
-# def bookmarks(self, context):
-#     print("\n» Setting Custom Bookmarks")
-
-#     path = bpy.utils.user_resource('CONFIG', "bookmarks.txt")
-
-#     lines = ['[Bookmarks]',
-#                 '!Archive',
-#                 '/home/x/Archive/blender',
-#                 '!TEMP',
-#                 '/home/x/TEMP/blender',
-#                 '!Addons',
-#                 '/home/x/TEMP/blender/Addons',
-#                 '!Output',
-#                 '/home/x/TEMP/blender/Output',
-#                 '[Recent]',
-#                 '!DECALmachine',
-#                 '/home/x/TEMP/blender/Addons/DECALmachine',
-#                 '!MESHmachine',
-#                 '/home/x/TEMP/blender/Addons/MESHmachine',
-#                 ]
-
-#     with open(path, mode='w') as f:
-#         f.write('\n'.join(lines))
