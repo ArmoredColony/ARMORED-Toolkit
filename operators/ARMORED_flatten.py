9b80a7bba2cc368aae53db3581c94089e524d736
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-04-24 19:28:37 -0600

    Update ARMORED_flatten.py
    Now prompts you to pick a projection plane before flattening the selected elements

--- operators/ARMORED_flatten.py
+++ operators/ARMORED_flatten.py
@@ -1,4 +1,5 @@
-version = (2, 0, 1)
+version = (2, 1, 0)
+
 
 import bpy
 import bmesh
@@ -7,99 +8,104 @@ import mathutils
 
 def get_items(self, context):
 	items = [ 
-		('AVERAGE',      'Average',      'Flattens to the average face normals.'), 
-		('NEAREST_AXIS', 'Nearest Axis', 'Flattens to the closest axis plane.'), 
-		('ACTIVE_FACE',  'Active Face',  'Flattens to the active face normal.'),
+		('AVERAGE',		'Average',		'Flattens to the average face normals.'), 
+		('NEAREST_ORTHO',	'Nearest Ortho',	'Flattens to the nearest orthographic plane.'), 
+		('ACTIVE_FACE',		'Active Face',		'Flattens to the active face normal.'),
 	]
-
-	if self.active_is_none:
-		del items[2]
 
 	return items
 
 
 class MESH_OT_armored_flatten(bpy.types.Operator):
-	'''Flattens your selection to the best-fitting imaginary plane.
+	'''Flattens your selection to an imaginary plane.
 
-armoredColony.com '''
+	armoredColony.com '''
 
 	bl_idname = 'mesh.armored_flatten'
 	bl_label = 'ARMORED Flatten'
 	bl_options = {'REGISTER', 'UNDO'}
 
-	alignment: bpy.props.EnumProperty(
-		name='Alignment', 
-		# default='AVERAGE', # Can't use this default since items come from a function?
-		description='Flatten selection to',
-		items=get_items)
+	projection_plane: bpy.props.EnumProperty(
+		name='Projection Plane', 
+		default=-1,
+		items=get_items,
+		options={'SKIP_SAVE'},
+	)
 	
 	active_is_none: bpy.props.BoolProperty(default=False, options={'SKIP_SAVE', 'HIDDEN'})
 		
 	def draw(self, context):
 		layout = self.layout
-		layout.label(text='Flattening Plane:')
+		layout.label(text='Projection Plane:')
 		row = layout.row()
-		row.prop(self, 'alignment', expand=True)
+		row.prop(self, 'projection_plane', expand=True)
 
 	@classmethod
 	def poll(cls, context):
 		return context.mode == 'EDIT_MESH'
+	
+	def invoke(self, context, event):
+		return context.window_manager.invoke_props_popup(self, event)
 
 	def execute(self, context):
-		# self.active_is_none = False	# FLAG TO CONTROL WHAT'S DRAWN IN THE REDO PANEL.
+		obj = context.edit_object
+		self.mesh = obj.data
+		self.bm = bmesh.from_edit_mesh(self.mesh)
+		self.bm.normal_update()
 
-		ob = context.edit_object
-		me = ob.data
-		bm = bmesh.from_edit_mesh(me)
-		bm.normal_update()
-
-		# selection_mode = context.tool_settings.mesh_select_mode[:]
-
-		sel_verts = {v for v in bm.verts if v.select}
-		sel_faces = [f for f in bm.faces if f.select]
+		sel_verts = [v for v in self.bm.verts if v.select]
+		sel_faces = {f for f in self.bm.faces if f.select}
 		
-		# active_face = bm.select_history.active
-		active_face = bm.faces.active	# THIS RETURNS THE ACTIVE FACE EVEN IF IT'S OUTSIDE THE CURRENT SELECTION.
-		
-		if active_face is None or active_face not in sel_faces:
-			self.active_is_none = True
-		
-		if self.alignment == 'AVERAGE':
+		active_face = self.bm.faces.active
 
-			target_normal = sum((f.normal for f in sel_faces), mathutils.Vector()).normalized()
-			target_center = sum((v.co for v in sel_verts), mathutils.Vector()) / len(list(sel_verts))
+		# Both modes use the same starting data.
+		if self.projection_plane in {'AVERAGE', 'NEAREST_ORTHO'}:
+			target_normal, target_center = self._average_normal_and_center(sel_faces, sel_verts)
+			
+	
+		if self.projection_plane == 'AVERAGE':
 
 			for v in sel_verts:
-				v.co = mathutils.geometry.intersect_line_plane(
-					v.co, v.co+target_normal, target_center, target_normal)
+				distance = (v.co - target_center).dot(target_normal)
+				v.co -= target_normal * distance
 
-		elif self.alignment == 'NEAREST_AXIS':
-
-			target_normal = sum((f.normal for f in sel_faces), mathutils.Vector()).normalized()
-			target_center = sum((v.co for v in sel_verts), mathutils.Vector()) / len(list(sel_verts))
-
+		elif self.projection_plane == 'NEAREST_ORTHO':
+			
+			# Finds the axis vector nearest to the average normal
+			# and applies the average vert.co location in that same axis to all verts.
 			angles = self._construct_angle_to_axis_dict(self._abs_vector(target_normal))
 			nearest_axis = min(angles, key=angles.get)
 
 			for v in sel_verts:
-				exec(f'v.co.{nearest_axis} = target_center.{nearest_axis}')
+				setattr(v.co, nearest_axis, getattr(target_center, nearest_axis))
 
-		elif self.alignment == 'ACTIVE_FACE':
+		elif self.projection_plane == 'ACTIVE_FACE':
+
+			if active_face is None or active_face not in sel_faces:
+				self.report({'WARNING'}, 'Active Face is None')
+				return {'FINISHED'}
 
 			target_normal = active_face.normal
 			target_center = active_face.calc_center_median()
 
-			plane = [v.co for v in active_face.verts[:3]]
-
 			for v in sel_verts:
-				v.co = mathutils.geometry.intersect_line_plane(
-					v.co, v.co+target_normal, plane[0], target_normal)
+				distance = (v.co - target_center).dot(target_normal)
+				v.co -= target_normal * distance
 
-		bm.normal_update()
-		bmesh.update_edit_mesh(me)
+		self.bm.normal_update()
+		bmesh.update_edit_mesh(self.mesh)
 
 		return {'FINISHED'}
 	
+	def _average_normal_and_center(self, faces, verts):
+		'''
+		Calculates the average normal and center of the selected faces and vertices.
+		'''
+		
+		target_normal = sum((f.normal for f in faces), mathutils.Vector()).normalized()
+		target_center = sum((v.co for v in verts), mathutils.Vector()) / len(verts)
+
+		return target_normal, target_center
 
 	def _abs_vector(self, vector: mathutils.Vector) -> mathutils.Vector:
 		'''
@@ -118,7 +124,7 @@ armoredColony.com '''
 			'x': vector.angle(mathutils.Vector((1, 0, 0))), 
 			'y': vector.angle(mathutils.Vector((0, 1, 0))), 
 			'z': vector.angle(mathutils.Vector((0, 0, 1)))
-			}
+		}
 
 
 classes = (
