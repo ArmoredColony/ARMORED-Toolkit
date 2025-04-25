0fb916e9cd8c8d75b38dbbaf0fd2520d143b6f2c
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-04-25 13:36:33 -0600

    Renamed and Modified node_stats
    Renamed ARMORED_node_stats.py to ARMORED_node_info.py
    
    Modified the printing logic to print out information in a cleaner and more readable way.

---
+++ operators/ARMORED_node_info.py
@@ -0,0 +1,72 @@
+version = (2, 0, 0)
+
+import bpy
+
+
+class NODE_OT_armored_node_info(bpy.types.Operator):
+	'''Get node info (name, label, type, inputs, outputs, etc).
+
+	armoredColony.com '''
+
+	bl_idname = 'node.armored_node_info'
+	bl_label = 'ARMORED Node Info'
+	bl_options = {'REGISTER', 'UNDO'}
+    
+
+	def execute(self, context):
+		node = context.active_node
+
+		if node is None:
+			self.report({'WARNING'}, 'Active Node is None')
+			return {'CANCELLED'}
+		
+		info = {
+			'name (uniq)':	node.name,
+			'label':	node.label,
+			'type':		node.type,
+			'bl_idname':	node.bl_idname,
+			'parent':	node.parent.name if node.parent else None,
+			'node_tree':	node.id_data.name,
+			'location':	node.location,
+		}
+
+		# Find the longest key for proper alignment
+		max_key_len = max(len(key) for key in (*info, 'inputs', 'outputs'))
+
+		separator = '='
+		tab_width = 8
+		tab = ' ' * tab_width
+
+		print('\nNODE INFO:')
+
+		for key, value in info.items():
+			space_for_non_string = ' ' if not isinstance(value, str) else ''
+			print(f'{tab}{key:<{max_key_len}} {separator} {space_for_non_string}{repr(value)}')
+
+		if node.inputs:
+			print(f'\tinputs:')
+			for i, val in enumerate(node.inputs):
+				print(f'{tab}{tab}{i:<{max_key_len - tab_width}} {separator} {repr(val.name)}')
+
+		if node.outputs:
+			print(f'\toutputs:')
+			for i, val in enumerate(node.outputs):
+				print(f'{tab}{tab}{i:<{max_key_len - tab_width}} {separator} {repr(val.name)}')
+		
+		print()
+
+		return {'FINISHED'}
+
+	
+	
+classes = (
+    NODE_OT_armored_node_info,
+)
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
