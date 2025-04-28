1646713b4b1af32e274546b3e22fd1db0ca01f4e
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-04-28 11:04:47 -0600

    Update ARMORED_fast_subdivision.py
    Added a `Pin to Last` property set to True and enabled Undo History to disabling `Pin to Last`

--- operators/ARMORED_fast_subdivision.py
+++ operators/ARMORED_fast_subdivision.py
@@ -1,4 +1,4 @@
-version = (2, 0, 1)
+version = (2, 1, 0)
 
 import bpy
 
@@ -8,17 +8,29 @@ class FastSubdivision:
 	Abstract Fast Subdivision class.
 	'''
 
-	bl_options = {'REGISTER'}
 	bl_description = 'Add a Subsurf Modifier with limit_surface quality set to 1 (faster but less accurate) \n\narmoredColony.com '
+	bl_options = {'REGISTER', 'UNDO'}
 	poll_mode = NotImplemented
 
 	level: bpy.props.IntProperty(
-		name='Level', default=1, min=0, soft_min=0, max=100, soft_max=6)
+		name='Level', 
+		default=1, 
+		min=0, 
+		soft_min=0, 
+		max=100, 
+		soft_max=6,
+	)
 
 	use_limit_surface: bpy.props.BoolProperty(
-		name='Use Limit Surface', default=True, 
+		name='Use Limit Surface', 
+		default=True, 
 		description='Disabled is faster for low SubD levels (1 or 2), but enabled is faster for higher levels (using my typical mesh resolution as an example)',
 		options={'SKIP_SAVE'},
+	)
+	
+	pin_to_last: bpy.props.BoolProperty(
+		name='Pin to Last',  
+		default=True,
 		)
 	
 	@classmethod
@@ -31,42 +43,45 @@ class FastSubdivision:
 
 		valid_objects = [obj for obj in context.selected_objects if obj.type in {'MESH'}]
 
-		for self.obj in valid_objects:
-			self._add_subsurf_modifier()
+		self._add_subsurf_modifier(objects=valid_objects)
 			
 		return {'FINISHED'}
 
 
-	def _add_subsurf_modifier(self) -> None:
-		mod = next((mod for mod in reversed(self.obj.modifiers) if mod.type == 'SUBSURF'), None)
+	# PRIVATE METHODS  __________________________________________________
 
-		if mod is None:
-			mod = self.obj.modifiers.new(name='Subdivision', type='SUBSURF')
+	def _add_subsurf_modifier(self, objects: list[bpy.types.Object]) -> None:
+		'''''
+		Adds a subsurf modifier to each object in the list.
+		'''
 
-		mod.use_limit_surface = self.use_limit_surface
-		mod.quality = 1
-		mod.levels = self.level
-		# mod.show_in_editmode = False
+		for obj in objects:
+			mod = next((mod for mod in reversed(obj.modifiers) if mod.type == 'SUBSURF'), None)
+
+			if mod is None:
+				mod = obj.modifiers.new(name='Subdivision', type='SUBSURF')
+
+			mod.use_limit_surface = self.use_limit_surface
+			mod.quality = 1
+			mod.levels = self.level
+			mod.use_pin_to_last = self.pin_to_last
+			# mod.show_in_editmode = False
 	
 	def _nothing_selected(self, context):
 		return not context.selected_objects
 		
 
 class OBJECT_OT_armored_fast_subdivision(bpy.types.Operator, FastSubdivision):
-
 	bl_idname = 'object.armored_fast_subdivision'
 	bl_label = 'ARMORED Fast Subdivision'
 	poll_mode = 'OBJECT'
 
 
 class MESH_OT_armored_fast_subdivision(bpy.types.Operator, FastSubdivision):
-
 	bl_idname = 'mesh.armored_fast_subdivision'
 	bl_label = 'ARMORED Fast Subdivision'
 	poll_mode = 'EDIT_MESH'
 	
-
-
 
 classes = (
 	OBJECT_OT_armored_fast_subdivision,
