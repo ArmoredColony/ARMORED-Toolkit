2e83d7d33c653ef8af0b6331c70aa0e302d36c0b
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-04-29 17:04:57 -0600

    Renamed addon to ARMORED Toolkit

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 bl_info = {
-    'name'        : 'Toolkit',
+    'name'        : 'ARMORED Toolkit',
     'description' : 'A collection of Scripts, Hotkeys and other goodies.',
     'author'      : 'Armored Colony',
-    'version'     : (0, 8, 3),
+    'version'     : (0, 8, 4),
     'blender'     : (2, 83),
     'location'    : 'Type "ARMORED" in the search menu.',
     # 'warning'     : 'Whatever', # Used for warning icon and text in addons panel.
@@ -10,7 +10,7 @@ bl_info = {
     'category'    : 'ARMORED',
 }
 
-debug = False
+DEBUG_MODE = False
 
 
 # FOLDERS WITH MODULES THAT CONTAIN <register> OR <unregister> FUNCTIONS (Operators, UI Panels, etc).
@@ -26,14 +26,14 @@ from . utils import mod_utils
 addon_modules = [module for folder in sub_folders for module in mod_utils.import_modules(folder)]
 
 if 'bpy' in locals():
-    mod_utils.reload_addon(debug)
+    mod_utils.reload_addon(DEBUG_MODE)
 
 import bpy
 
 
 def register():
-    mod_utils.register_modules(addon_modules, debug)
+    mod_utils.register_modules(addon_modules, DEBUG_MODE)
 
 
 def unregister():
-    mod_utils.unregister_modules(reversed(addon_modules), debug)
+    mod_utils.unregister_modules(reversed(addon_modules), DEBUG_MODE)
