319056583f01688bd96b2b71a78293da1f4034e5
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-04-29 17:05:21 -0600

    Using new Web Icons

--- utils/icons.py
+++ utils/icons.py
@@ -7,17 +7,27 @@ from . import paths
 def load_icons(preview_collections):
 	pcoll = bpy.utils.previews.new()
 
-	pcoll.load('armored_colony32',	os.path.join(paths.AddonPaths.icons, 'armored_colony32.png'),	'IMAGE')
-	pcoll.load('artstation32',	os.path.join(paths.AddonPaths.icons, 'artstation32.png'),	'IMAGE')
-	pcoll.load('blender_market32',	os.path.join(paths.AddonPaths.icons, 'blender_market32.png'),	'IMAGE')
-	pcoll.load('github32',		os.path.join(paths.AddonPaths.icons, 'github32.png'),		'IMAGE')
-	pcoll.load('youtube32',		os.path.join(paths.AddonPaths.icons, 'youtube32.png'),		'IMAGE')
+	icon_path  = paths.AddonPaths.icons
+
+	icon_names = [	# Without the PNG extension.
+		'armored_colony32',
+		'artstation32',
+		'blender_market32',
+		'github32',
+		'gumroad32',
+		'youtube32',
+	]
+
+	for icon_name in icon_names:
+		pcoll.load(icon_name, os.path.join(icon_path, f'{icon_name}.png'), 'IMAGE')
 
 	preview_collections['web_icons'] = pcoll
 
 	return preview_collections
 
+
 def unload_icons(preview_collections):
 	for pcoll in preview_collections.values():
 		bpy.utils.previews.remove(pcoll)
+		
 	preview_collections.clear()
