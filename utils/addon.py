ae371d779dfccfab105e08e5c613fb76b556de3b
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-04-29 17:05:33 -0600

    Whitespace

--- utils/addon.py
+++ utils/addon.py
@@ -7,14 +7,18 @@ import os
 def path():
     return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+
 def name():
     return __package__.split('.')[0]
+
 
 def addon():
     return bpy.context.preferences.addons[name()]
 
+
 def prefs():
     return addon().preferences
+
 
 def debug():
     return prefs().debug
