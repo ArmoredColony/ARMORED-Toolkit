9be63594343a9c0664d44a3b4dfcb5616ad6e323
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-04-29 17:06:02 -0600

    Added Docstrings

--- customize/resources.py
+++ customize/resources.py
@@ -2,72 +2,82 @@ import bpy
 import os
 
 from .. utils import (
-    addon,
-    files,
-    paths,
+	addon,
+	files,
+	paths,
 )
 
 
 class Resources():
-    SOURCE_PATH = NotImplemented
-    TARGET_PATH = NotImplemented
+	'''
+	Abstract class for resources that are loaded/unloaded by copying 
+	or deleting files from the addon folder to the blender folder.
+	'''
+	
+	SOURCE_PATH = NotImplemented
+	TARGET_PATH = NotImplemented
 
-    @classmethod
-    def load(cls):
-        files.copy_files(cls.SOURCE_PATH, cls.TARGET_PATH)
+	@classmethod
+	def load(cls):
+		files.copy_files(cls.SOURCE_PATH, cls.TARGET_PATH)
 
-    @classmethod
-    def unload(cls):
-        files.delete_files(cls.TARGET_PATH, file_names=os.listdir(cls.SOURCE_PATH))
+	@classmethod
+	def unload(cls):
+		files.delete_files(cls.TARGET_PATH, file_names=os.listdir(cls.SOURCE_PATH))
 
 
 class StudioResources(Resources):
-    @classmethod
-    def load(cls):
-        super().load()
-        bpy.context.preferences.studio_lights.refresh()
+	'''
+	Expands the load/unload methods of the Resource class, 
+	to refresh the sudio_lights panel.
+	'''
+	
+	@classmethod
+	def load(cls):
+		super().load()
+		bpy.context.preferences.studio_lights.refresh()
 
-    @classmethod
-    def unload(cls):
-        super().unload()
-        bpy.context.preferences.studio_lights.refresh()
+	@classmethod
+	def unload(cls):
+		super().unload()
+		bpy.context.preferences.studio_lights.refresh()
 
 
 # Use upercase names for classes that will be called by updating their correspoding 
 # property in the addon preferences (prop: studio_lights class_name: STUDIO_LIGHTS).
 
 class THEMES(Resources):
-    SOURCE_PATH = paths.AddonPaths.themes
-    TARGET_PATH = paths.BlenderPaths.themes
+	SOURCE_PATH = paths.AddonPaths.themes
+	TARGET_PATH = paths.BlenderPaths.themes
 
 
 class MATCAPS(StudioResources):
-    SOURCE_PATH = paths.AddonPaths.matcaps
-    TARGET_PATH = paths.BlenderPaths.matcaps
+	SOURCE_PATH = paths.AddonPaths.matcaps
+	TARGET_PATH = paths.BlenderPaths.matcaps
 
 
 class HDRIS(StudioResources):
-    SOURCE_PATH = paths.AddonPaths.hdris
-    TARGET_PATH = paths.BlenderPaths.hdris
+	SOURCE_PATH = paths.AddonPaths.hdris
+	TARGET_PATH = paths.BlenderPaths.hdris
 
 
 class STUDIO_LIGHTS(StudioResources):
-    SOURCE_PATH = paths.AddonPaths.studio_lights
-    TARGET_PATH = paths.BlenderPaths.studio_lights
+	SOURCE_PATH = paths.AddonPaths.studio_lights
+	TARGET_PATH = paths.BlenderPaths.studio_lights
 
 
 def register():
-    prefs = addon.prefs()
-
-    MATCAPS.load()       if prefs.matcaps       else MATCAPS.unload()
-    HDRIS.load()         if prefs.hdris         else HDRIS.unload()
-    STUDIO_LIGHTS.load() if prefs.studio_lights else STUDIO_LIGHTS.unload()
-    THEMES.load()        if prefs.themes        else THEMES.unload()
+	prefs = addon.prefs()
+	
+	MATCAPS.load()       if prefs.matcaps       else MATCAPS.unload()
+	HDRIS.load()         if prefs.hdris         else HDRIS.unload()
+	STUDIO_LIGHTS.load() if prefs.studio_lights else STUDIO_LIGHTS.unload()
+	THEMES.load()        if prefs.themes        else THEMES.unload()
 
 
 def unregister():
-    if isinstance(bpy.context.space_data, bpy.types.SpacePreferences):
-        MATCAPS.unload()
-        HDRIS.unload()
-        STUDIO_LIGHTS.unload()
-        THEMES.unload()
+	if isinstance(bpy.context.space_data, bpy.types.SpacePreferences):
+		MATCAPS.unload()
+		HDRIS.unload()
+		STUDIO_LIGHTS.unload()
+		THEMES.unload()
