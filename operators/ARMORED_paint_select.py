b639172e9d0af1ad6c8c2b0a4c6005287372365c
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-04-29 11:38:56 -0600

    Update ARMORED_paint_select.py
    Inconsequential changes like whitespace and variable name

--- operators/ARMORED_paint_select.py
+++ operators/ARMORED_paint_select.py
@@ -1,66 +1,70 @@
-import bpy
-# from bpy.props import IntProperty, FloatProperty
+version = (1, 0, 0)
 
-# v0.6
+import bpy
+
 
 class ARMORED_OT_paint_select(bpy.types.Operator):
-    '''Paint Select like Modo (bind it to a single key, like "Q") (Experimental)
+	'''Paint Select like Modo (bind it to a single key, like "Q") (Experimental)
 
-(www.armoredColony.com)'''
+	armoredColony.com '''
 
-    bl_idname = 'view3d.armored_paint_select'
-    bl_label = 'ARMORED Paint Select'
-    bl_options = {'REGISTER', 'UNDO'}
+	bl_idname = 'view3d.armored_paint_select'
+	bl_label = 'ARMORED Paint Select'
+	bl_options = {'REGISTER', 'UNDO'}
 
-    def invoke(self, context, event):
-        self.painted = False
-        self.selecting = False
-        self.deselecting = False
-        self.first_key = event.type
+	def invoke(self, context, event):
+		self.painted = False
+		self.selecting = False
+		self.deselecting = False
+		self.first_key_pressed = event.type
 
-        context.window_manager.modal_handler_add(self)
-        return {'RUNNING_MODAL'}
+		context.window_manager.modal_handler_add(self)
+		
+		return {'RUNNING_MODAL'}
 
-    def modal(self, context, event):
+	def modal(self, context, event):
 
-        if event.type == 'LEFTMOUSE':
-            if event.value == 'PRESS':
-                self.selecting = True
-                self.painted = True
-            else:
-                self.selecting = False
+		if event.type == 'LEFTMOUSE':
 
-        if event.type == 'RIGHTMOUSE':
-            if event.value == 'PRESS':
-                self.deselecting = True
-            else:
-                self.deselecting = False
+			if event.value == 'PRESS':
+				self.selecting = True
+				self.painted = True
+			else:
+				self.selecting = False
 
-        if event.type == self.first_key and event.value == 'RELEASE':    
-            if not self.painted:
-                bpy.ops.wm.tool_set_by_id(name='builtin.select_box')
-            return {'FINISHED'}
 
-        elif event.type in {'ESC'}:
-            return {'CANCELLED'}
-        
-        if self.selecting:
-            bpy.ops.view3d.select('INVOKE_DEFAULT', extend=True, deselect=False)
-        if self.deselecting:
-            bpy.ops.view3d.select('INVOKE_DEFAULT', extend=False, deselect=True)
+		elif event.type == 'RIGHTMOUSE':
+				self.deselecting = True if event.value == 'PRESS' else False
 
-        return {'RUNNING_MODAL'}
+
+		elif event.type == self.first_key_pressed and event.value == 'RELEASE':  
+
+			if not self.painted:
+				bpy.ops.wm.tool_set_by_id(name='builtin.select_box', cycle=False)
+
+			return {'FINISHED'}
+
+		elif event.type in {'ESC'}:
+			return {'CANCELLED'}
+	
+		if self.selecting:
+			bpy.ops.view3d.select('INVOKE_DEFAULT', extend=True, deselect=False)
+
+		if self.deselecting:
+			bpy.ops.view3d.select('INVOKE_DEFAULT', extend=False, deselect=True)
+
+		return {'RUNNING_MODAL'}
 
 
 classes = (
-    ARMORED_OT_paint_select,
+	ARMORED_OT_paint_select,
 )
 
 def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)    
+	for cls in classes:
+		bpy.utils.register_class(cls)    
 
 
 def unregister():
-    for cls in classes:
-        bpy.utils.unregister_class(cls)
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
