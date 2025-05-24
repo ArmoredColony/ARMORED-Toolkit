8035418ad5dcd401c1adbdef0023d954242f7f21
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-05-23 20:25:04 -0600

    Added Commentary

--- operators_internal/ARMORED_wireframe_selected.py
+++ operators_internal/ARMORED_wireframe_selected.py
@@ -1,4 +1,4 @@
-version = (2, 0, 0)
+version = (2, 0, 1)
 
 import bpy
 
@@ -7,7 +7,13 @@ from .. utils import (
 	debug,
 )
 
+
 last_selection = []
+
+
+# The reason we use all these functions is because we have to disable the object wireframe
+# when the wireframe_selected property is set to False. In other words, it is not enough
+# to simply remove the handlers; we have to perform a "cleanup".
 
 
 def enable_wireframe(objects):
@@ -45,7 +51,7 @@ def wireframe_selected_objects(*args):
 		enable_wireframe(last_selection)
 
 
-@bpy.app.handlers.persistent
+# @bpy.app.handlers.persistent
 def add_load_post_handler():
 	'''
 	Add a `load_post` handler that in turn adds a `depsgraph_update_post` handler.
@@ -64,7 +70,7 @@ def remove_load_post_handler():
 		bpy.app.handlers.load_post.remove(add_depsgraph_handler)
 		debug.msg('ARMORED_wireframe_selected: Removed Load Post Handler.')
 
-	except RuntimeError:
+	except Exception:
 		debug.msg('ARMORED_wireframe_selected: Could not Remove Load Post Handler.')
 		pass
 
@@ -72,7 +78,7 @@ def remove_load_post_handler():
 @bpy.app.handlers.persistent
 def add_depsgraph_handler(*args):
 	'''
-	Update the `last_selection` global and add a `depsgraph_update_post` that controls
+	Update `last_selection` global and add a `depsgraph_update_post` that controls
 	wireframe visualization.
 	'''
 	
@@ -87,7 +93,7 @@ def add_depsgraph_handler(*args):
 
 def remove_depsgraph_handler(*args):
 	'''
-	Update the `last_selection` global and attempt to remove the `depsgraph_update_post` handler.
+	Update `last_selection` global and attempt to remove the `depsgraph_update_post` handler.
 	'''
 	
 	global last_selection
@@ -98,21 +104,9 @@ def remove_depsgraph_handler(*args):
 		bpy.app.handlers.depsgraph_update_post.remove(wireframe_selected_objects)
 		debug.msg('ARMORED_wireframe_selected: Removed Depsgraph Handler')
 
-	except RuntimeError:
+	except Exception:
 		debug.msg('ARMORED_wireframe_selected: Could not Unregister Depsgraph Handler.')
-
 	
-# class ARMORED_OT_wireframe_selected(bpy.types.Operator):
-# 	bl_idname = 'armored.wireframe_selected'
-# 	bl_label = 'ARMORED Wireframe Selected'
-# 	bl_options = {'REGISTER', 'INTERNAL'}
-
-# 	def execute(self, context):
-# 		objects = context.selected_objects
-# 		enable_wireframe(objects)
-		
-# 		return {'FINISHED'}
-
 
 def register():
 	if addon.prefs().wireframe_selected:
@@ -122,8 +116,3 @@ def register():
 def unregister():
 	remove_load_post_handler()
 	remove_depsgraph_handler()
-
-
-# import bpy
-# print(bpy.app.handlers.load_post[:])
-# print(bpy.app.handlers.depsgraph_update_post[:])
