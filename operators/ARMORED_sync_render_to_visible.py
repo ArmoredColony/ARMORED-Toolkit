6c0e1c94142d9b05fc3c232c22ea7cf59127fa93
Armored Colony <78882317+ArmoredColony@users.noreply.github.com>
2025-06-02 14:03:14 -0600

    Now syncs Collection Viewport Visibility
    collection.hide_viewport is the global viewport visibility.
    
    layer_collection.collection..hide_viewport is the eyeball.
    
    The first is now linked with the eyeball as well.

--- operators/ARMORED_sync_render_to_visible.py
+++ operators/ARMORED_sync_render_to_visible.py
@@ -1,4 +1,4 @@
-version = (1, 2, 0)
+version = (1, 3, 0)
 
 import bpy
 
@@ -16,8 +16,8 @@ def get_view_layer_collections(context):
 	'''
 
 	return [
-		lc for lc in traverse_tree(context.view_layer.layer_collection)
-		if lc.collection.name != 'Scene Collection'
+		layer_coll for layer_coll in traverse_tree(context.view_layer.layer_collection)
+		if layer_coll.collection.name != 'Scene Collection'
 	]
 
 
@@ -34,10 +34,10 @@ def get_all_scene_objects(context):
 
 
 @bpy.app.handlers.persistent
-def sync_render_to_viewport(*args):
+def sync_render_to_viewport(self, context):
 	'''
-	Check if the sync_render Scene property is enabled before
-	syncing the render visibility to the viewport visibility.
+	If the sync_render Scene property is enabled,
+	sync the render and global visibility to the viewport visibility.
 	'''
 	
 	sync_render = bpy.context.scene.armored_sync_render
@@ -46,17 +46,25 @@ def sync_render_to_viewport(*args):
 		return
 
 	print('ARMORED-Toolkit: Syncing Render to Viewport visibility')
+
+	# A layer collection is the instance of a collection in a specifif view layer.
 	layer_collections = get_view_layer_collections(bpy.context)
 	scene_objects     = get_all_scene_objects(bpy.context)
 
 	for layer_coll in layer_collections:
-		layer_coll.collection.hide_render = layer_coll.hide_viewport
+		layer_coll.collection.hide_render   = layer_coll.hide_viewport
+		layer_coll.collection.hide_viewport = layer_coll.hide_viewport
 
 	for obj in scene_objects:
 		obj.hide_render = obj.hide_get()
-
+		obj.hide_viewport = obj.hide_get()	# This is the global viewport visibility
+	
 
 def draw_sync_button(self, context):
+	'''
+	Draws an button you can toggle.
+	'''
+	
 	self.layout.prop(context.scene, 'armored_sync_render', icon='RESTRICT_RENDER_OFF', text='')
 
 
@@ -64,11 +72,19 @@ def register():
 	bpy.types.Scene.armored_sync_render = bpy.props.BoolProperty(
 		name='ARMORED Sync Render to Viewport', 
 		default=False,
-		description='When starting a render, automatically sync it to the Viewport object visibility',
+		update=sync_render_to_viewport,
+		description=(
+			'When this toggle is enabled, the Render and Viewport visibility '
+			'will sync with the outliner eyeball. \n\n'
+			'Keep it enabled for automatic syncing when starting a render.'
+		),
 	)
+
 	bpy.app.handlers.render_pre.append(sync_render_to_viewport)
 	bpy.types.OUTLINER_HT_header.append(draw_sync_button)
+
 
 def unregister():
 	bpy.app.handlers.render_pre.remove(sync_render_to_viewport)
 	bpy.types.OUTLINER_HT_header.remove(draw_sync_button)
+	del bpy.types.Scene.armored_sync_render
